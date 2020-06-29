b89666d58d9b96af8dcec74a68f2208d202f5145
ThomasWilliamWallace <thomas.wallace@machineswithvision.com>
2020-06-29 20:56:57 +0100

    Tested predict steps with zero duration.

--- src/tests.cpp
+++ src/tests.cpp
@@ -6,10 +6,11 @@
 #include "measurement_package.h"
 #include "Eigen/Dense"
 #include "ukf.h"
+#include <cassert>
 
 
 // Checks that UKF state is unchanged by a prediction of zero duration.
-bool ZeroTimePredict() {
+bool ZeroDurationPredict() {
     UKF ukf;
 
     // Feed in first measurement
@@ -27,22 +28,37 @@ bool ZeroTimePredict() {
     // Predict state after 0 time
     ukf.Prediction(0);
 
-    // Verify that the UKF state is unchanged
     Eigen::VectorXd x_diff = x_prev - ukf.x_;
     Eigen::MatrixXd P_diff = P_prev - ukf.P_;
-    std::cout << "x_prev=\n" << x_prev << "\n";
-    std::cout << "ukf.x_=\n" << ukf.x_ << "\n";
-    std::cout << "x_diff=\n" << x_diff << "\n";
-    std::cout << "P_prev=\n" << P_prev << "\n";
-    std::cout << "ukf.P_=\n" << ukf.P_ << "\n";
-    std::cout << "P_diff=\n" << P_diff << "\n";
+
+    bool x_mean_changed = x_diff.isMuchSmallerThan(1e7);
+    assert(("Mean state unchanged between initialization and zero-duration predict step.", x_mean_changed));
+
+    bool P_mean_changed = P_diff.isMuchSmallerThan(1e7);
+    assert(("Mean covariance matrix unchanged between initialization and zero-duration predict step.", P_mean_changed));
+
+    // Store UKF state
+    x_prev = ukf.x_;
+    P_prev = ukf.P_;
+
+    // Predict state after 0 time
+    ukf.Prediction(0);
+
+    x_diff = x_prev - ukf.x_;
+    P_diff = P_prev - ukf.P_;
+
+    x_mean_changed = x_diff.isMuchSmallerThan(1e10);
+    assert(("Mean state unchanged between first and second zero-duration predict step.", x_mean_changed));
+
+    P_mean_changed = P_diff.isMuchSmallerThan(1e10);
+    assert(("Mean covariance matrix unchanged between first and second zero-duration predict step.", P_mean_changed));
 
     return false;
 };
 
 void RunAllTests() {
     std::cout << "Running all tests\n";
-    ZeroTimePredict();
+    ZeroDurationPredict();
 };
 
 int main(int argc, char** argv) {
