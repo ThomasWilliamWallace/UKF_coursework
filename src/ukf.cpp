400ea0bb0fabcdd9c56148a5795ff1e7f0fbafe1
ThomasWilliamWallace <thomas.wallace@machineswithvision.com>
2020-06-29 21:31:35 +0100

    Indendation

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -10,85 +10,85 @@ using Eigen::VectorXd;
  * Initializes Unscented Kalman filter
  */
 UKF::UKF() {
-  // if this is false, laser measurements will be ignored (except during init)
-  use_laser_ = true;
+    // if this is false, laser measurements will be ignored (except during init)
+    use_laser_ = true;
 
-  // if this is false, radar measurements will be ignored (except during init)
-  use_radar_ = true;
+    // if this is false, radar measurements will be ignored (except during init)
+    use_radar_ = true;
 
-  n_x_ = 5;
-  n_aug_ = 7;
-  n_sigma_ = 1 + 2 * n_aug_;
+    n_x_ = 5;
+    n_aug_ = 7;
+    n_sigma_ = 1 + 2 * n_aug_;
 
-  // initial state vector
-  x_ = VectorXd::Zero(n_x_);
-  x_aug_ = VectorXd::Zero(n_aug_);
+    // initial state vector
+    x_ = VectorXd::Zero(n_x_);
+    x_aug_ = VectorXd::Zero(n_aug_);
 
-  // initial covariance matrix
-  P_ = MatrixXd::Identity(n_x_, n_x_);
-  P_aug_ = MatrixXd::Zero(n_aug_, n_aug_);
+    // initial covariance matrix
+    P_ = MatrixXd::Identity(n_x_, n_x_);
+    P_aug_ = MatrixXd::Zero(n_aug_, n_aug_);
 
-  Xsig_pred_ = MatrixXd::Zero(n_x_, n_sigma_);
+    Xsig_pred_ = MatrixXd::Zero(n_x_, n_sigma_);
 
-  // Process noise standard deviation longitudinal acceleration in m/s^2
-  std_a_ = 3;
+    // Process noise standard deviation longitudinal acceleration in m/s^2
+    std_a_ = 3;
 
-  // Process noise standard deviation yaw acceleration in rad/s^2
-  std_yawdd_ = (M_PI * 3 / 6) / 2;
-  
-  /**
-   * DO NOT MODIFY measurement noise values below.
-   * These are provided by the sensor manufacturer.
-   */
+    // Process noise standard deviation yaw acceleration in rad/s^2
+    std_yawdd_ = (M_PI * 3 / 6) / 2;
 
-  // Laser measurement noise standard deviation position1 in m
-  std_laspx_ = 0.15;
+    /**
+     * DO NOT MODIFY measurement noise values below.
+     * These are provided by the sensor manufacturer.
+     */
 
-  // Laser measurement noise standard deviation position2 in m
-  std_laspy_ = 0.15;
+    // Laser measurement noise standard deviation position1 in m
+    std_laspx_ = 0.15;
 
-  // Radar measurement noise standard deviation radius in m
-  std_radr_ = 0.3;
+    // Laser measurement noise standard deviation position2 in m
+    std_laspy_ = 0.15;
 
-  // Radar measurement noise standard deviation angle in rad
-  std_radphi_ = 0.03;
+    // Radar measurement noise standard deviation radius in m
+    std_radr_ = 0.3;
 
-  // Radar measurement noise standard deviation radius change in m/s
-  std_radrd_ = 0.3;
-  
-  /**
-   * End DO NOT MODIFY section for measurement noise values 
-   */
-  
-  /**
-   * TODO: Complete the initialization. See ukf.h for other member properties.
-   * Hint: one or more values initialized above might be wildly off...
-   */
-   lambda_ = 3 - n_aug_;
-   weights_ = Eigen::VectorXd(n_sigma_);
-   weights_(0) = lambda_ / (lambda_ + n_aug_);
-   for (int sigma = 1; sigma < weights_.size(); sigma++) {
-       weights_(sigma) = 1 / (2 * (lambda_ + n_aug_));
-   }
-   sigma_points_ = Eigen::MatrixXd::Zero(n_aug_, n_sigma_);
+    // Radar measurement noise standard deviation angle in rad
+    std_radphi_ = 0.03;
+
+    // Radar measurement noise standard deviation radius change in m/s
+    std_radrd_ = 0.3;
+
+    /**
+     * End DO NOT MODIFY section for measurement noise values
+     */
+
+    /**
+     * TODO: Complete the initialization. See ukf.h for other member properties.
+     * Hint: one or more values initialized above might be wildly off...
+     */
+    lambda_ = 3 - n_aug_;
+    weights_ = Eigen::VectorXd(n_sigma_);
+    weights_(0) = lambda_ / (lambda_ + n_aug_);
+    for (int sigma = 1; sigma < weights_.size(); sigma++) {
+        weights_(sigma) = 1 / (2 * (lambda_ + n_aug_));
+    }
+    sigma_points_ = Eigen::MatrixXd::Zero(n_aug_, n_sigma_);
 }
 
 UKF::~UKF() {}
 
 void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
-  /**
-   * TODO: Complete this function! Make sure you switch between lidar and radar
-   * measurements.
-   */
-  //  std::cout << "UKF::ProcessMeasurement, x_=" << x_ << "\n";
-  Prediction((meas_package.timestamp_ - timestamp_) / 1000000.0);
-  if (meas_package.sensor_type_ == meas_package.LASER) {
-      UpdateLidar(meas_package);
-  } else {
-      UpdateRadar(meas_package);
-  }
-  timestamp_ = meas_package.timestamp_;
-  is_initialized_ = true;
+    /**
+     * TODO: Complete this function! Make sure you switch between lidar and radar
+     * measurements.
+     */
+    //  std::cout << "UKF::ProcessMeasurement, x_=" << x_ << "\n";
+    Prediction((meas_package.timestamp_ - timestamp_) / 1000000.0);
+    if (meas_package.sensor_type_ == meas_package.LASER) {
+        UpdateLidar(meas_package);
+    } else {
+        UpdateRadar(meas_package);
+    }
+    timestamp_ = meas_package.timestamp_;
+    is_initialized_ = true;
 }
 
 //VectorXd z_diff = Zsig.col(sigma_index) - z_pred;
@@ -110,11 +110,11 @@ double NormaliseAngle(double angle) {
 }
 
 void UKF::Prediction(double delta_t) {
-  /**
-   * TODO: Complete this function! Estimate the object's location. 
-   * Modify the state vector, x_. Predict sigma points, the state, 
-   * and the state covariance matrix.
-   */
+    /**
+     * TODO: Complete this function! Estimate the object's location.
+     * Modify the state vector, x_. Predict sigma points, the state,
+     * and the state covariance matrix.
+     */
     std::cout << "\nUKF::Prediction\n";
     if (!is_initialized_) {
         return;
@@ -323,12 +323,12 @@ Eigen::VectorXd UKF::LidarMeasurementFunction(MeasurementPackage meas_package) {
 }
 
 void UKF::UpdateLidar(MeasurementPackage meas_package) {
-  /**
-   * TODO: Complete this function! Use lidar data to update the belief 
-   * about the object's position. Modify the state vector, x_, and 
-   * covariance, P_.
-   * You can also calculate the lidar NIS, if desired.
-   */
+    /**
+     * TODO: Complete this function! Use lidar data to update the belief
+     * about the object's position. Modify the state vector, x_, and
+     * covariance, P_.
+     * You can also calculate the lidar NIS, if desired.
+     */
     Eigen::VectorXd measured_state = LidarMeasurementFunction(meas_package);
 
     if (is_initialized_) {
@@ -362,24 +362,24 @@ Eigen::VectorXd UKF::RadarMeasurementFunction(MeasurementPackage meas_package) {
 }
 
 void UKF::UpdateRadar(MeasurementPackage meas_package) {
-  /**
-   * TODO: Complete this function! Use radar data to update the belief 
-   * about the object's position. Modify the state vector, x_, and 
-   * covariance, P_.
-   * You can also calculate the radar NIS, if desired.
-   */
-  Eigen::VectorXd measured_state = RadarMeasurementFunction(meas_package);
+    /**
+     * TODO: Complete this function! Use radar data to update the belief
+     * about the object's position. Modify the state vector, x_, and
+     * covariance, P_.
+     * You can also calculate the radar NIS, if desired.
+     */
+    Eigen::VectorXd measured_state = RadarMeasurementFunction(meas_package);
 
-  if (is_initialized_) {
-      // Apply measurement model to sigma points
+    if (is_initialized_) {
+        // Apply measurement model to sigma points
 
-      // Calculate mean and covariance from sigma points
+        // Calculate mean and covariance from sigma points
 
-  } else {
-      x_ << measured_state;
-      x_(2) = 10;
+    } else {
+        x_ << measured_state;
+        x_(2) = 10;
       x_(3) = M_PI/2;
       x_(4) = M_PI/5;
 //      P_ << 0;
-  }
+    }
 }
